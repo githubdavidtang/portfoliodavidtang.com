e46ed18264a229bc3f84bfd960b1e137d9c02958
Julien Vernet <julien@vernet.me>
2014-12-16 15:42:51 +0700

    Add fallback for Google CDN

--- includes/footer-scripts.php
+++ includes/footer-scripts.php
@@ -1,10 +1,11 @@
-<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
-<script type="text/javascript" src="js/jquery.supersized.3.1.3.min.js"></script>
+<script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script type="text/javascript">window.jQuery || document.write('<script src="js/vendor/jquery.min.js"><\/script>')</script>
 <script type="text/javascript">
 $(function () {
 	$('#toggle').click(function () {
 		$(this).toggleClass('active');
 		$('#menu').slideToggle('slow');
+		return false;
 	});
 });
 </script>

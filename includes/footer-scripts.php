9bd87ffa5c96ac7b23bc3e303f2877090b7efce5
Julien Vernet <julien@vernet.me>
2014-12-16 16:20:39 +0700

    Export javascript functions and parameters for better control over the ouput (minify...)

--- includes/footer-scripts.php
+++ includes/footer-scripts.php
@@ -1,12 +1,4 @@
 <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
 <script type="text/javascript">window.jQuery || document.write('<script src="js/vendor/jquery.min.js"><\/script>')</script>
-<script type="text/javascript" src="js/jquery.supersized.3.1.3.min.js"></script>
-<script type="text/javascript">
-$(function () {
-	$('#toggle').click(function () {
-		$(this).toggleClass('active');
-		$('#menu').slideToggle('slow');
-		return false;
-	});
-});
-</script>
+<script type="text/javascript" src="js/vendor/jquery.supersized.3.1.3.min.js"></script>
+<script type="text/javascript" src="js/app-dist.js"></script>

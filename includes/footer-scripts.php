0c40ed985eadb9770cd01a1dc3be29db0e1c79d8
Julien Vernet <julien@vernet.me>
2014-12-16 11:44:21 +0700

    Wrap JS functions in document.ready statement

--- includes/footer-scripts.php
+++ includes/footer-scripts.php
@@ -1,10 +1,10 @@
 <script type="text/javascript" src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 <script type="text/javascript" src="js/jquery.supersized.3.1.3.min.js"></script>
 <script type="text/javascript">
-$('#toggle').click(function() {
-	$('#toggle').toggleClass("active");
-	$('#menu').slideToggle('slow', function() {
-		// Animation complete.
+$(function () {
+	$('#toggle').click(function () {
+		$(this).toggleClass('active');
+		$('#menu').slideToggle('slow');
 	});
 });
 </script>

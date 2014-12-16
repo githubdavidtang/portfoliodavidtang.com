4f9c0007edb4c13b7dc055bcb4a90fc118e5e041
Julien Vernet <julien@vernet.me>
2014-12-16 15:47:05 +0700

    Update footer credits

--- includes/copyright.php
+++ includes/copyright.php
@@ -1,4 +1,4 @@
-	<div class="copyright">
-		<span>&copy; Copyright David Tang</span>
-		<span><a href="http://julienvernet.info" target="_blank">Code by Julien Vernet</a></span>
-	</div>
+<div class="copyright">
+	<span>&copy; Copyright David Tang</span>
+	<span><a href="http://n2clic.com" target="_blank">Code by N2Clic Ltd</a></span>
+</div>

bd29cf6fbdf4d578ac49fcae4f628295e3ccfcf5
Julien Vernet <julien@vernet.me>
2014-12-16 16:37:54 +0700

    Remove unused footer

--- includes/footer-homepage.php
+++
@@ -1,7 +0,0 @@
-	<div id="footer" class="sidebar">
-	<h2>Contact</h2>
-		<ul>
-			<li><a href="mailto:emaildavidtang@gmail.com">emaildavidtang@gmail.com</a></li>
-			<li><a href="javascript:void(0)">Skype: skypedavidtang</a></li>
-		</ul>
-	</div>

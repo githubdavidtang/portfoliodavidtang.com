6cb551d1dc0e648099d30fd258d4a9aab940e6d3
SiamKreative <julien@vernet.me>
2015-07-10 11:50:17 +0700

    Re-organize includes (3 files now instead of 6) & Update navigation menu

--- _includes/copyright.php
+++
@@ -1,4 +0,0 @@
-<div class="copyright">
-	<span>&copy; Copyright David Tang</span>
-	<span><a href="http://n2clic.com" target="_blank">Code by N2Clic Ltd</a></span>
-</div>
--- _includes/header.php
+++
@@ -1,7 +0,0 @@
-<meta charset="utf-8">
-<meta http-equiv="X-UA-Compatible" content="IE=edge">
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" type="text/css" href="css/style.css">
-
-<!-- Google Analytics -->
-<script type="text/javascript">!function(e,t,a,n,c,o){e.GoogleAnalyticsObject=n,e[n]||(e[n]=function(){(e[n].q=e[n].q||[]).push(arguments)}),e[n].l=+new Date,c=t.createElement(a),o=t.getElementsByTagName(a)[0],c.src="//www.google-analytics.com/analytics.js",o.parentNode.insertBefore(c,o)}(window,document,"script","ga"),ga("create","UA-15485830-32","auto"),ga("send","pageview");</script>
